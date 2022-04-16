e2f3236cf048cb1cee98d03e7c45d18655c5db35
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-16 17:59:40 -0300

    update

--- main.js
+++ main.js
@@ -3,15 +3,9 @@
 const openModal = () => document.getElementById('modal')
   .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-  .classList.remove('active')
-
-
-const tempClient = {
-  nome: "Capelari",
-  email: "capelari@gmail.com",
-  celular: "19997075772",
-  cidade: "São José do Rio Pardo"
+const closeModal = () => {
+  clearFields()
+  document.getElementById('modal').classList.remove('active')
 }
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
@@ -38,9 +32,64 @@ const createClient = (client) => {
   setLocalStorage(dbClient)
 }
 
+const isValidFields = () => {
+  return document.getElementById('form').reportValidity()
+}
+
+// Interação com o layout
+
+const clearFields = () => {
+  const fields = document.querySelectorAll('.modal-field')
+  fields.forEach(field => field.value = '')
+}
+
+const saveClient = () => {
+  if (isValidFields()) {
+    const client = {
+      nome: document.getElementById('nome').value,
+      email: document.getElementById('email').value,
+      celular: document.getElementById('celular').value,
+      cidade: document.getElementById('cidade').value
+    }
+    createClient(client)
+    closeModal()
+  }
+}
+
+const createRow = (client) => {
+  const newRow = document.createElement('tr')
+  newRow.innerHTML = `
+     <td>${client.nome}</td>
+     <td>${client.email}</td>
+     <td>${client.celular}</td>
+     <td>${client.cidade}</td>
+     <td>
+        <button type="button" class="button green">editar</button>
+        <button type="button" class="button red">excluir</button>
+     </td>
+  `
+  document.querySelector('#tableClient>tbody').appendChild(newRow)
+}
+
+const clearTable = () => {
+  const rows = document.querySelectorAll('#tableClient>tbody tr')
+  rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const updateTable = () => {
+  const dbClient = readClient()
+  clearTable()
+  dbClient.forEach(createRow)
+}
+
+updateTable()
+
 // Eventos
 document.getElementById('cadastrarCliente')
   .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
   .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+  .addEventListener('click', saveClient)
