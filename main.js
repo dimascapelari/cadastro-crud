56910937f3cf29918c62c45577c5c06ea2692880
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-16 16:46:30 -0300

    updates

--- main.js
+++ main.js
@@ -8,16 +8,34 @@ const closeModal = () => document.getElementById('modal')
 
 
 const tempClient = {
-  nome: "Dimas",
-  email: "dima.capelari@gmail.com",
+  nome: "Capelari",
+  email: "capelari@gmail.com",
   celular: "19997075772",
   cidade: "São José do Rio Pardo"
 }
 
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
 // CRUD - create read update delete
-
-const createCliente = (cliente) => {
-
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index, 1)
+  setLocalStorage(dbClient)
+}
+// CRUD UPDATE
+const updateClient = (index, client) => {
+  const dbClient = readClient()
+  dbClient[index] = client
+  setLocalStorage(dbClient)
+}
+// CRUD - READ
+const readClient = () => getLocalStorage()
+// CRUD - CREATE
+const createClient = (client) => {
+  const dbClient = getLocalStorage()
+  dbClient.push(client)
+  setLocalStorage(dbClient)
 }
 
 // Eventos
